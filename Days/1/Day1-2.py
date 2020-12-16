951f0866853824e34f50739a19bf929f15f4a656
Billy300000 <nathaniel.c.ferre@gmail.com>
2020-12-16 15:39:59 -0700

    Finished Day 1 part 2 and added the input file and a shell command file I used to run the python scripts

---
+++ Days/1/Day1-2.py
@@ -0,0 +1,20 @@
+# Day 1 - Part 1 of Advent of Code 2020
+# Objective for this part:
+#   Find 2 numbers that add up to 2020 in input.txt and multiply them for the answer
+
+input_file = open("input.txt", "r")
+lines = input_file.readlines()   # Saves each line in an array with each index being individual lines
+
+# Change the input array to integer values to be used later
+for x in range(len(lines)):
+    lines[x] = int(lines[x].strip())
+
+for i in range(len(lines)):
+    for j in range(len(lines)-i):
+        for k in range(len(lines)-j):
+            if(lines[i] + lines[j] + lines[k] == 2020):
+                total = lines[i] * lines[j] * lines[k]
+
+print("The total is: " + str(total))
+                
+input_file.close()
---
+++ Days/1/py.sh
@@ -0,0 +1,7 @@
+if [ $1 -eq 1 ]
+then
+    python3 Day1-1.py
+elif [ $1 -eq 2 ]
+then
+    python3 Day1-2.py
+fi
