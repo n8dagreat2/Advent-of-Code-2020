0bf6273695a682045e794cd6bb48d37e2811f85f
Billy300000 <nathaniel.c.ferre@gmail.com>
2020-12-16 14:54:04 -0700

    Completed the first part of the first day

--- Days/1/Day1-1.py
+++ Days/1/Day1-1.py
@@ -1,3 +1,18 @@
-input_line = input()
+# Day 1 - Part 1 of Advent of Code 2020
+# Objective for this part:
+#   Find 2 numbers that add up to 2020 in input.txt and multiply them for the answer
 
-print(input_line)
+input_file = open("input.txt", "r")
+lines = input_file.readlines()   # Saves each line in an array with each index being individual lines
+
+# Change the input array to integer values to be used later
+for x in range(len(lines)):
+    lines[x] = int(lines[x].strip())
+
+for i in range(len(lines)):
+    for j in range(len(lines) - i):
+        if(lines[i] + lines[i + j] == 2020):
+            total = lines[i] * lines[i + j]
+            print("The total is: " + str(total))
+
+input_file.close()
